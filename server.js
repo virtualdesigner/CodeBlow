01ac5fff213445a104453e6b7b2fefcd4db36e99
virtualdesigner <30769960+virtualdesigner@users.noreply.github.com>
2018-07-06 18:10:51 +0530

    ready for deployment (with some changes in server.js to make sure the app was working)

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -16,10 +17,14 @@ app.use(bodyParser.json());
 app.use("/api/posts", posts);
 app.use("/api/qa", qa);
 
-// test
-app.get("/", (req, res) => {
-  res.json({ msg: "Hey there!" });
-});
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set Static Folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 mongoose
   .connect(db)
